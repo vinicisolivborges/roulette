15da3ea657f77deed0cbd3fed938da37485ede97
Vinicius O Borges <viniciusolivborges@gmail.com>
2023-02-20 18:05:28 -0300

    V7

--- src/app/components/analyzes/analyzes.component.ts
+++ src/app/components/analyzes/analyzes.component.ts
@@ -16,6 +16,7 @@ export class AnalyzesComponent implements OnInit {
   public analyzes: Array<any> = [];
   public sequencePull: Array<any> = [];
   public sequencePullNeighbor: Array<any> = [];
+  public region: Array<any> = [];
 
   public analyzesNumber: Array<any> = [];
   public analyzesNeighbor: Array<any> = [];
@@ -59,6 +60,379 @@ export class AnalyzesComponent implements OnInit {
     [3, 12],
     [13, 11]
   ];
+
+  public numbers = [
+    {
+        number: 1
+      , color: 1
+      , section: 2
+      , neighbor: [16,33,20,14]
+      , column: 1
+      , dozen: 1
+      , high: 0
+      , odd: 1
+    },
+    {
+        number: 2
+      , color: 2
+      , section: 1
+      , neighbor: [4, 21, 25, 17]
+      , column: 2
+      , dozen: 1
+      , high: 0
+      , odd: 0
+    },
+    {
+        number: 3
+      , color: 1
+      , section: 1
+      , neighbor: [26, 0, 35, 12]
+      , column: 3
+      , dozen: 1
+      , high: 0
+      , odd: 1
+    },
+    {
+        number: 4
+      , color: 2
+      , section: 1
+      , neighbor: [15, 19, 21, 2]
+      , column: 1
+      , dozen: 1
+      , high: 0
+      , odd: 0
+    },
+    {
+        number: 5
+      , color: 1
+      , section: 3
+      , neighbor: [23, 10, 24, 16]
+      , column: 2
+      , dozen: 1
+      , high: 0
+      , odd: 1
+    },
+    {
+        number: 6
+      , color: 2
+      , section: 2
+      , neighbor: [17, 34, 27,13]
+      , column: 3
+      , dozen: 1
+      , high: 0
+      , odd: 0
+    },
+    {
+        number: 7
+      , color: 1
+      , section: 1
+      , neighbor: [18, 29, 28, 12]
+      , column: 1
+      , dozen: 1
+      , high: 0
+      , odd: 1
+    },
+    {
+        number: 8
+      , color: 2
+      , section: 3
+      , neighbor: [11, 30, 23, 10]
+      , column: 2
+      , dozen: 1
+      , high: 0
+      , odd: 0
+    },
+    {
+        number: 9
+      , color: 1
+      , section: 2
+      , neighbor: [14, 31, 22, 18]
+      , column: 3
+      , dozen: 1
+      , high: 0
+      , odd: 1
+    },
+    {
+        number: 10
+      , color: 2
+      , section: 3
+      , neighbor: [8, 23, 5, 24]
+      , column: 1
+      , dozen: 1
+      , high: 0
+      , odd: 0
+    },
+    {
+        number: 11
+      , color: 2
+      , section: 3
+      , neighbor: [13, 36, 30, 8]
+      , column: 2
+      , dozen: 1
+      , high: 0
+      , odd: 1
+    },
+    {
+        number: 12
+      , color: 1
+      , section: 1
+      , neighbor: [7, 28, 35, 3]
+      , column: 3
+      , dozen: 1
+      , high: 0
+      , odd: 0
+    },
+    {
+        number: 13
+      , color: 2
+      , section: 3
+      , neighbor: [6, 27, 36, 11]
+      , column: 1
+      , dozen: 2
+      , high: 0
+      , odd: 1
+    },
+    {
+        number: 14
+      , color: 1
+      , section: 2
+      , neighbor: [1, 20, 31, 9]
+      , column: 2
+      , dozen: 2
+      , high: 0
+      , odd: 0
+    },
+    {
+        number: 15
+      , color: 2
+      , section: 1
+      , neighbor: [0, 32, 19, 4]
+      , column: 3
+      , dozen: 2
+      , high: 0
+      , odd: 1
+    },
+    {
+        number: 16
+      , color: 1
+      , section: 3
+      , neighbor: [5, 24, 33, 1]
+      , column: 1
+      , dozen: 2
+      , high: 0
+      , odd: 0
+    },
+    {
+        number: 17
+      , color: 2
+      , section: 2
+      , neighbor: [2, 25, 34, 6]
+      , column: 2
+      , dozen: 2
+      , high: 0
+      , odd: 1
+    },
+    {
+        number: 18
+      , color: 1
+      , section: 1
+      , neighbor: [9, 22, 29, 7]
+      , column: 3
+      , dozen: 2
+      , high: 0
+      , odd: 0
+    },
+    {
+        number: 19
+      , color: 1
+      , section: 1
+      , neighbor: [32, 15, 21, 2]
+      , column: 1
+      , dozen: 2
+      , high: 1
+      , odd: 1
+    },
+    {
+        number: 20
+      , color: 2
+      , section: 2
+      , neighbor: [33, 1, 14, 31]
+      , column: 2
+      , dozen: 2
+      , high: 1
+      , odd: 0
+    },
+    {
+        number: 21
+      , color: 1
+      , section: 1
+      , neighbor: [19, 4, 2, 25]
+      , column: 3
+      , dozen: 2
+      , high: 1
+      , odd: 1
+    },
+    {
+        number: 22
+      , color: 2
+      , section: 1
+      , neighbor: [31, 9, 18, 29]
+      , column: 1
+      , dozen: 2
+      , high: 1
+      , odd: 0
+    },
+    {
+        number: 23
+      , color: 1
+      , section: 3
+      , neighbor: [30, 8, 10, 5]
+      , column: 2
+      , dozen: 2
+      , high: 1
+      , odd: 1
+    },
+    {
+        number: 24
+      , color: 2
+      , section: 3
+      , neighbor: [10, 5, 16, 33]
+      , column: 3
+      , dozen: 2
+      , high: 1
+      , odd: 0
+    },
+    {
+        number: 25
+      , color: 1
+      , section: 1
+      , neighbor: [21, 2, 17, 34]
+      , column: 1
+      , dozen: 3
+      , high: 1
+      , odd: 1
+    },
+    {
+        number: 26
+      , color: 2
+      , section: 1
+      , neighbor: [35, 3, 0, 32]
+      , column: 2
+      , dozen: 3
+      , high: 1
+      , odd: 0
+    },
+    {
+        number: 27
+      , color: 1
+      , section: 3
+      , neighbor: [34, 6, 27, 13]
+      , column: 3
+      , dozen: 3
+      , high: 1
+      , odd: 1
+    },
+    {
+        number: 28
+      , color: 2
+      , section: 1
+      , neighbor: [29, 7, 12, 35]
+      , column: 1
+      , dozen: 3
+      , high: 1
+      , odd: 0
+    },
+    {
+        number: 29
+      , color: 2
+      , section: 1
+      , neighbor: [22, 18, 7, 28]
+      , column: 2
+      , dozen: 3
+      , high: 1
+      , odd: 1
+    },
+    {
+        number: 30
+      , color: 1
+      , section: 3
+      , neighbor: [36, 11, 8, 23]
+      , column: 3
+      , dozen: 3
+      , high: 1
+      , odd: 0
+    },
+    {
+        number: 31
+      , color: 2
+      , section: 2
+      , neighbor: [20, 14, 9, 22]
+      , column: 1
+      , dozen: 3
+      , high: 1
+      , odd: 1
+    },
+    {
+        number: 32
+      , color: 1
+      , section: 1
+      , neighbor: [26, 0, 15, 19]
+      , column: 2
+      , dozen: 3
+      , high: 1
+      , odd: 0
+    },
+    {
+        number: 33
+      , color: 2
+      , section: 3
+      , neighbor: [24, 16, 1, 20]
+      , column: 3
+      , dozen: 3
+      , high: 1
+      , odd: 1
+    },
+    {
+        number: 34
+      , color: 1
+      , section: 2
+      , neighbor: [25, 17, 6, 27]
+      , column: 1
+      , dozen: 3
+      , high: 1
+      , odd: 0
+    },
+    {
+        number: 35
+      , color: 2
+      , section: 1
+      , neighbor: [28, 12, 3, 26]
+      , column: 2
+      , dozen: 3
+      , high: 1
+      , odd: 1
+    },
+    {
+        number: 36
+      , color: 1
+      , section: 3
+      , neighbor: [27, 13, 11, 30]
+      , column: 3
+      , dozen: 3
+      , high: 1
+      , odd: 0
+    },
+    {
+        number: 0
+      , color: 0
+      , section: 1
+      , neighbor: [3, 26, 32, 15]
+      , column: 0
+      , dozen: 0
+      , high: 0
+      , odd: 0
+    }
+  ]
 
   constructor() { }
 
@@ -338,7 +712,7 @@ export class AnalyzesComponent implements OnInit {
         ]
       }
     ];
-
+    this.region_numbers(this.sequencePull);
     this.analyzes_emit.emit(this.analyzes);
   }
 
@@ -514,5 +888,29 @@ export class AnalyzesComponent implements OnInit {
     ];
     
     this.analyzes_emit.emit(this.analyzes);
+  }
+
+  region_numbers = (number) => {
+    this.region = [];
+    const numeros = number.map(n => { return n.number });
+    let aaa:Array<any> = [];
+    numeros.map(value => this.numbers.map(n => {
+      if(n.neighbor.includes(value)) {
+        aaa.push(n.number);
+      }
+    }));
+    const occurrences = aaa.reduce((acc, curr) => {
+      return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
+    }, {});
+    const keys = Object.keys(occurrences);
+
+    keys.map(k => {
+      const n = this.numbers.filter(n => n.number == parseInt(k));
+      n[0][`total`] = occurrences[k];
+      this.region.push(n[0]);
+    })
+    console.log(this.region);
+
+    
   }
 }
